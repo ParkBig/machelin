21fe94a654729a0e5b4ae0d5536a94236f71fe6f
ParkBig <enfantgu@gmail.com>
2024-01-01 02:24:27 +0900

    add netInfo function

--- src/components/bottomTabScreen/mainScreen/restaurants/Restaurants.tsx
+++ src/components/bottomTabScreen/mainScreen/restaurants/Restaurants.tsx
@@ -1,17 +1,30 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import { useRecoilValue } from 'recoil';
 import { mainScreenTogglesState } from 'store/toggleState';
 import RestaurantList from './restaurantList/RestaurantList';
 import RepresentationRestaurantInfo from './representationRestaurantInfo/RepresentationRestaurant';
 import FunctionsBar from './functionsBar/FunctionsBar';
+import { useNetInfo } from '@react-native-community/netinfo';
+import { Colors } from 'const/global-styles';
 
 export default function Restaurants() {
+  const netInfo = useNetInfo();
   const { toggleRestaurantList } = useRecoilValue(mainScreenTogglesState);
 
   return (
     <View style={styles.wrap}>
       <FunctionsBar />
-      {toggleRestaurantList ? <RestaurantList /> : <RepresentationRestaurantInfo />}
+      {netInfo.isConnected ? (
+        toggleRestaurantList ? (
+          <RestaurantList />
+        ) : (
+          <RepresentationRestaurantInfo />
+        )
+      ) : (
+        <View style={styles.netInfo}>
+          <Text>인터넷 연결이 불안정합니다</Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -22,5 +35,12 @@ const styles = StyleSheet.create({
     position: 'absolute',
     zIndex: 2,
     bottom: 0,
+  },
+  netInfo: {
+    width: '100%',
+    paddingVertical: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.mainWhite1,
   },
 });
