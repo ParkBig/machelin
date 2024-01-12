72af3024ef8092a700a73d776e8497736dada138
ParkBig <enfantgu@gmail.com>
2024-01-13 00:33:30 +0900

    modify IPost postType

--- src/screens/router/TopTabScreen.tsx
+++ src/screens/router/TopTabScreen.tsx
@@ -1,3 +1,5 @@
+import { Colors } from 'const/global-styles';
+import { Platform, StatusBar } from 'react-native';
 import NoticePostsScreen from 'screens/topTab/NoticePostsScreen';
 import PromotionPostsScreen from 'screens/topTab/PromotionPostsScreen';
 import SearchPostsScreen from 'screens/topTab/SearchPostsScreen';
@@ -5,10 +7,20 @@ import { RootTobTab } from 'types/screenType';
 
 export default function TopTabScreen() {
   return (
-    <RootTobTab.Navigator>
-      <RootTobTab.Screen name="SearchPostsScreen" component={SearchPostsScreen} />
-      <RootTobTab.Screen name="NoticePostsScreen" component={NoticePostsScreen} />
-      <RootTobTab.Screen name="PromotionPostsScreen" component={PromotionPostsScreen} />
+    <RootTobTab.Navigator
+      style={{
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+        backgroundColor: Colors.mainGreen2,
+      }}
+      screenOptions={{
+        tabBarStyle: { backgroundColor: Colors.mainGreen2 },
+        tabBarActiveTintColor: Colors.mainWhite3,
+        tabBarLabelStyle: { fontWeight: 'bold' },
+      }}
+    >
+      <RootTobTab.Screen name="SearchPostsScreen" component={SearchPostsScreen} options={{ title: '게시글 검색' }} />
+      <RootTobTab.Screen name="NoticePostsScreen" component={NoticePostsScreen} options={{ title: '공지사항' }} />
+      <RootTobTab.Screen name="PromotionPostsScreen" component={PromotionPostsScreen} options={{ title: '프로모션' }} />
     </RootTobTab.Navigator>
   );
 }
