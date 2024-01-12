6805de4eee190bf71afec1d2bde49e7bbd56aa6f
ParkBig <enfantgu@gmail.com>
2024-01-12 17:13:37 +0900

    modify Post (delete props isDetailScreen)

--- src/components/common/card/post/Post.tsx
+++ src/components/common/card/post/Post.tsx
@@ -11,13 +11,12 @@ import CreatedAt from './createdAt/CreatedAt';
 
 interface Props {
   posts: IPost;
-  isDetailScreen: boolean;
 }
 
-export default function Post({ posts, isDetailScreen }: Props) {
+export default function Post({ posts }: Props) {
   return (
     <View style={styles.wrap}>
-      <PostBriefRestaurantInfo posts={posts} isDetailScreen={isDetailScreen} />
+      <PostBriefRestaurantInfo posts={posts} />
       <Writer posts={posts} />
       <CreatedAt createdAt={posts.createdAt} />
       <PostingImages images={posts.images} />
