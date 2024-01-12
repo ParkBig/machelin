32e9199c02352afe44ff868607e0d3387c8445c3
ParkBig <enfantgu@gmail.com>
2024-01-13 04:44:03 +0900

    V 13(1.0.2)

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ config();
 const expoConfig: ExpoConfig = {
   name: '마슐랭',
   slug: 'machelin',
-  version: '1.0.128',
+  version: '1.0.2',
   orientation: 'portrait',
   icon: './src/assets/icon.png',
   userInterfaceStyle: 'light',
@@ -17,11 +17,11 @@ const expoConfig: ExpoConfig = {
   },
   assetBundlePatterns: ['**/*'],
   ios: {
-    buildNumber: '1.0.128',
+    buildNumber: '1.0.2',
     supportsTablet: true,
   },
   android: {
-    versionCode: 12,
+    versionCode: 13,
     adaptiveIcon: {
       foregroundImage: './src/assets/adaptive-icon.png',
       backgroundColor: '#ffffff',
