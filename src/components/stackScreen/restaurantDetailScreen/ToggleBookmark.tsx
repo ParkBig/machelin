aa62fc26db596deb8fe040cfcdbddb0302ae6b93
ParkBig <enfantgu@gmail.com>
2024-01-27 13:42:56 +0900

    add LoadingOverlay to ToggleBookmark

--- src/components/stackScreen/restaurantDetailScreen/ToggleBookmark.tsx
+++ src/components/stackScreen/restaurantDetailScreen/ToggleBookmark.tsx
@@ -1,12 +1,14 @@
 import { Ionicons } from '@expo/vector-icons';
 import Button from 'components/common/layout/Button';
 import ConfirmAlertModal, { ToggleState } from 'components/common/modal/ConfirmAlertModal';
+import LoadingOverlay from 'components/common/modal/LoadingOverlay';
 import { Colors } from 'const/global-styles';
 import { toggleBookmarkQuery } from 'query/api/bookmarks';
 import useRestaurantDetailQuery from 'query/hooks/restaurants/useRestaurantDetailQuery';
 import useMyInfoQuery from 'query/hooks/users/useMyInfoQuery';
 import useUsersBookmarksQuery from 'query/hooks/users/useUsersBookmarksQuery';
 import { useState } from 'react';
+import { StyleSheet } from 'react-native';
 import { useMutation } from 'react-query';
 import imageTranslator from 'util/imageTranslator';
 
@@ -20,7 +22,7 @@ export default function ToggleBookmark({ restaurantId }: Props) {
   const { bookmarks, reBookmarks } = useUsersBookmarksQuery();
   const [toggleAlertModal, setToggleAlertModal] = useState<ToggleState>({ toggle: false, alertMsg: '' });
 
-  const { mutate } = useMutation(toggleBookmarkQuery, {
+  const { mutate, isLoading } = useMutation(toggleBookmarkQuery, {
     onSettled: res => {
       if (res.ok) {
         reMyInfo();
@@ -77,9 +79,13 @@ export default function ToggleBookmark({ restaurantId }: Props) {
 
   return (
     <>
-      <Button onPress={toggleBookmarkHandler}>
-        <Ionicons name={isMyBookmark ? 'bookmark' : 'bookmark-outline'} size={25} color={Colors.mainWhite3} />
-      </Button>
+      {isLoading ? (
+        <LoadingOverlay style={styles.loadingOverlay} size={30} color={Colors.mainWhite3} />
+      ) : (
+        <Button onPress={toggleBookmarkHandler}>
+          <Ionicons name={isMyBookmark ? 'bookmark' : 'bookmark-outline'} size={25} color={Colors.mainWhite3} />
+        </Button>
+      )}
       <ConfirmAlertModal
         toggleModal={toggleAlertModal.toggle}
         setToggleAlertModal={setToggleAlertModal}
@@ -88,3 +94,10 @@ export default function ToggleBookmark({ restaurantId }: Props) {
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  loadingOverlay: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
