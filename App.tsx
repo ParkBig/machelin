e032a99dfa026464d2aff5e5203883025e950b7c
ParkBig <enfantgu@gmail.com>
2024-02-23 22:39:53 +0900

    add version check

--- App.tsx
+++ App.tsx
@@ -6,12 +6,27 @@ import { useForegroundPermissions } from 'expo-location';
 import { useEffect } from 'react';
 import { verifyLocationPermissions, verifyMediaLibraryPermissions } from 'util/accessRight';
 import { useMediaLibraryPermissions } from 'expo-image-picker';
+import { Alert } from 'react-native';
+import { createAxiosInstance } from 'query/api/api';
 
 const queryClient = new QueryClient();
 
 export default function App() {
   const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
   const [mediaLibraryPermissionInfo, reqMediaLibraryPermission] = useMediaLibraryPermissions();
+
+  useEffect(() => {
+    const versionCheck = async () => {
+      const axiosVersions = createAxiosInstance('versions');
+      const { data } = await axiosVersions.get('');
+
+      if (data.machelinCurrentVersion !== '1.0.403') {
+        Alert.alert('업데이트 알림', '원활한 사용을 위해 업데이트 해주세요');
+      }
+    };
+
+    versionCheck();
+  }, []);
 
   useEffect(() => {
     (async () => {
