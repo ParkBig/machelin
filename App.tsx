cc3e5f4ff87b17e459aafb03a984af257d280d6b
ParkBig <enfantgu@gmail.com>
2024-03-17 02:49:13 +0900

    V 23(1.0.405)

--- App.tsx
+++ App.tsx
@@ -23,7 +23,7 @@ export default function App() {
         const axiosVersions = createAxiosInstance('versions');
         const { data } = await axiosVersions.get('');
   
-        if (data.machelinCurrentVersion !== '1.0.404') {
+        if (data.machelinCurrentVersion !== '1.0.405') {
           Alert.alert('업데이트 알림', '원활한 사용을 위해 업데이트 해주세요');
         }
       }
